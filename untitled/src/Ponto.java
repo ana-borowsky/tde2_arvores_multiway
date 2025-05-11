fc8a5471754b5a1236de252de52385be10abc64a
Gusta Dev <gustacamara.dev@hotmail.com>
2025-05-11 13:10:37 -0300

    implementa ponto

---
+++ untitled/src/Ponto.java
@@ -0,0 +1,25 @@
+public class Ponto {
+    private int xPosicao;
+    private int yPosicao;
+
+    public Ponto(int xPosicao, int yPosicao) {
+        this.xPosicao = xPosicao;
+        this.yPosicao = yPosicao;
+    }
+
+    public int getxPosicao() {
+        return xPosicao;
+    }
+
+    public void setxPosicao(int xPosicao) {
+        this.xPosicao = xPosicao;
+    }
+
+    public int getyPosicao() {
+        return yPosicao;
+    }
+
+    public void setyPosicao(int yPosicao) {
+        this.yPosicao = yPosicao;
+    }
+}
